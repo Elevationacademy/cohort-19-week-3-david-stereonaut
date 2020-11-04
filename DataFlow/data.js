f4ad1ecc9b2f8345ce86353c5e63f5e20307b769
david.stereonaut <adonbarnash@gmail.com>
2020-11-04 17:34:07 +0200

    something had to be done

--- DataFlow/data.js
+++ DataFlow/data.js
@@ -2,4 +2,5 @@ const posts = [
     {name: "Holy Giant Lobster", text: "Help me molt and I'll make all of your dreams come true"},
     {name: "Holy Giant Spaghetti Monster", text: "As the old god, I realize my reign has ended... Please help the new god to molt"},
     {name: "God", text: "Everyday mankind strays further from the light..."}
+    {name: "Father", text: "I wish you would stop hanging out with those weird guys"}
 ]
